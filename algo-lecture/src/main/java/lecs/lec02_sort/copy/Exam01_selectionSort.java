680be4d42fd74ca7bd756d8f55cfbf9e0f23be90
pparksuuu <supr2000@gmail.com>
2018-08-11 22:57:12 +0900

    .

---
+++ algo-lecture/src/main/java/lecs/lec02_sort/copy/Exam01_selectionSort.java
@@ -0,0 +1,12 @@
+package lecs.lec02_sort.copy;
+
+public class Exam01_selectionSort {
+    public static void main(String args[]) {
+       func();
+    }
+    
+    public static void func() {
+        System.out.println("Hello...");
+        func();
+    } 
+}
