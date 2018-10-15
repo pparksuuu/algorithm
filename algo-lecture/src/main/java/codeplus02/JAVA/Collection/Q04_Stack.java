98e4b1cc0707be063a6866a3e777389f043dcb76
pparksuuu <supr2000@gmail.com>
2018-10-15 20:06:40 +0900

    Algo Practice

--- algo-lecture/src/main/java/codeplus02/JAVA/Collection/Q04_Stack.java
+++ algo-lecture/src/main/java/codeplus02/JAVA/Collection/Q04_Stack.java
@@ -19,6 +19,38 @@ import java.util.*;
 
 public class Q04_Stack {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine(); // 정수 뒤에 \n이 있어서 필요합니다
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int k=0; k<n; k++) {
+            String line = sc.nextLine();
+            String[] s = line.split(" ");
+            String cmd = s[0];
+            if (cmd.equals("push")) {
+                int num = Integer.parseInt(s[1]);
+                stack.push(num);
+            } else if (cmd.equals("top")) {
+                if (stack.empty()) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(stack.peek());
+                }
+            } else if (cmd.equals("size")) {
+                System.out.println(stack.size());
+            } else if (cmd.equals("empty")) {
+                if (stack.empty()) {
+                    System.out.println("1");
+                } else {
+                    System.out.println("0");
+                }
+            } else if (cmd.equals("pop")) {
+                if (stack.empty()) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(stack.pop());
+                }
+            }
+        }
     }
 }
